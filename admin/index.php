d502d1eff79ebb98cb04727ee19e6b27152f1718
Ananya Trivedi <ananyatrivedi070@gmail.com>
2021-11-11 18:33:02 +0530

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,51 @@
+
+<?php include('partials/menu.php'); ?>
+
+<!-- Main Content Section Starts -->
+<div class="main-content">
+            <div class="wrapper">
+                <h1>Dashboard</h1>
+                <br><br>
+                <br><br>
+
+<div class="col-4 text-center">
+
+   
+
+    <h1>Categories</h1>
+    <br />
+    
+</div>
+
+<div class="col-4 text-center">
+
+   
+
+    <h1> Foods</h1>
+    <br />
+   
+</div>
+
+<div class="col-4 text-center">
+    
+   
+    <h1>Total Orders</h1>
+    <br />
+    
+</div>
+
+<div class="col-4 text-center">
+    
+    
+    <h1>Revenue Generated<h1>
+    <br />
+    
+</div>
+
+<div class="clearfix"></div>
+
+</div>
+</div>
+<!-- Main Content Setion Ends -->
+
+<?php include('partials/footer.php') ?>
---
+++ admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2020 All rights reserved,Developed By - <a href="#">Ananya Trivedi</a></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
